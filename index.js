9e86ec32d83c244717f5e867fe266f1c30f46022
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-04-18 08:17:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,96 +9,107 @@ function redirect(linkNumber) {
       window.location.replace("https://angulorecto.github.io/int-link/memberLinks.html");
    }
    if (linkNumber == 1) {
-     let key = prompt("Please insert your id key.");
-      if (key == "Icy8X8ZSM4") {
-         window.open("https://silver-chinchilla-boot.cyclic.app/");
+      let key = prompt("Please insert your id key.");
+      if (key == "") {
+         window.location.replace("https://angulorecto.github.io/int-link/");
+      } else if (key != "Icy8X8ZSM4") {
+         window.location.replace("https://angulorecto.github.io/int-link/");
+      } else {
+         window.location.replace("https://silver-chinchilla-boot.cyclic.app/");
       }
    }
    if (linkNumber == 2) {
-     window.open("https://child.cyclic.app/");
+      let key = prompt("Please insert your id key.");
+      if (key == "") {
+         window.location.replace("https://angulorecto.github.io/int-link/");
+      } else if (key != "Icy8X8ZSM4") {
+         window.location.replace("https://angulorecto.github.io/int-link/");
+      } else {
+         window.location.replace("https://child.cyclic.app/");
+      }
    }
    if (linkNumber == 3) {
-     window.open("https://pear-cautious-kingfisher.cyclic.app/");
+     window.location.replace("https://pear-cautious-kingfisher.cyclic.app/");
    }
    if (linkNumber == 4) {
-     window.open("https://budget-consensus-unaware.onrender.com/");
+     window.location.replace("https://budget-consensus-unaware.onrender.com/");
    }
   if (linkNumber == 5) {
-    window.open("https://yummyfood-tk.vercel.app/");
+    window.location.replace("https://yummyfood-tk.vercel.app/");
   }
   if (linkNumber == 6) {
-    window.open("https://head-egfc.onrender.com/");
+    window.location.replace("https://head-egfc.onrender.com/");
   }
   if (linkNumber == 7) {
-    window.open("https://overcharge.onrender.com/");
+    window.location.replace("https://overcharge.onrender.com/");
   }
   if (linkNumber == 8) {
-    window.open("https://project-wte8.onrender.com/");
+    window.location.replace("https://project-wte8.onrender.com/");
   }
   if (linkNumber == 9) {
-    window.open("https://ignite-q595.onrender.com/");
+    window.location.replace("https://ignite-q595.onrender.com/");
   }
   if (linkNumber == 10) {
-    window.open("https://certain-snap-venture-research.onrender.com/");
+    window.location.replace("https://certain-snap-venture-research.onrender.com/");
   }
   if (linkNumber == 11) {
-    window.open("https://refer-perforate-extreme-loop.onrender.com/");
+    window.location.replace("https://refer-perforate-extreme-loop.onrender.com/");
   }
   if (linkNumber == 12) {
-    window.open("https://contradiction-movement-state-ghostwriter.onrender.com/");
+    window.location.replace("https://contradiction-movement-state-ghostwriter.onrender.com/");
   }
   if (linkNumber == 13) {
-    window.open("https://assault-oppose-virtue-tree.onrender.com/");
+    window.location.replace("https://assault-oppose-virtue-tree.onrender.com/");
   }
   if (linkNumber == 14) {
-    window.open("https://accent-threshold-farewell.onrender.com/");
+    window.location.replace("https://accent-threshold-farewell.onrender.com/");
   }
   if (linkNumber == 15) {
-    window.open("https://committee-myth-repeat.onrender.com/");
+    window.location.replace("https://committee-myth-repeat.onrender.com/");
   }
   if (linkNumber == 16) {
-    window.open("https://contrast-adventure-pause.onrender.com/");
+    window.location.replace("https://contrast-adventure-pause.onrender.com/");
   }
   if (linkNumber == 17) {
-    window.open("https://ethan-is-cool.onrender.com/");
+    window.location.replace("https://ethan-is-cool.onrender.com/");
   }
   if (linkNumber == 18) {
-    window.open("https://floor-confuse-sample.onrender.com/");
+    window.location.replace("https://floor-confuse-sample.onrender.com/");
   }
   if (linkNumber == 19) {
-    window.open("https://promotion-suppress-mine.onrender.com/");
+    window.location.replace("https://promotion-suppress-mine.onrender.com/");
   }
   if (linkNumber == 20) {
-    window.open("https://constitution-commemorate-navy.onrender.com/");
+    window.location.replace("https://constitution-commemorate-navy.onrender.com/");
   }
   if (linkNumber == 21) {
-    window.open("https://exploration-federation-unfortunate.onrender.com/");
+    window.location.replace("https://exploration-federation-unfortunate.onrender.com/");
   }
   if (linkNumber == 22) {
-    window.open("https://engineer-aunt-technology.onrender.com/");
+    window.location.replace("https://engineer-aunt-technology.onrender.com/");
   }
   if (linkNumber == 23) {
-    window.open("https://undertake-recruit-instrument.onrender.com/");
+    window.location.replace("https://undertake-recruit-instrument.onrender.com/");
   }
   if (linkNumber == 24) {
-    window.open("https://rare-penetrate-authorise.onrender.com/");
+    window.location.replace("https://rare-penetrate-authorise.onrender.com/");
   }
   if (linkNumber == 25) {
-    window.open("https://promise-rugby-grudge.onrender.com/");
+    window.location.replace("https://promise-rugby-grudge.onrender.com/");
   }
   if (linkNumber == 26) {
-    window.open("https://dough-lemon-despair.onrender.com/");
+    window.location.replace("https://dough-lemon-despair.onrender.com/");
   }
   if (linkNumber == 27) {
-    window.open("https://ancestor-vein-proposal.onrender.com/");
+    window.location.replace("https://ancestor-vein-proposal.onrender.com/");
   }
   if (linkNumber == 28) {
-    window.open("https://drift-widen-courtesy.onrender.com/");
+    window.location.replace("https://drift-widen-courtesy.onrender.com/");
   }
   if (linkNumber == 29) {
-    window.open("https://side-finished-tumble.onrender.com/");
+    window.location.replace("https://side-finished-tumble.onrender.com/");
   }
   if (linkNumber == 30) {
-    window.open("https://push-owner-patient.onrender.com/");
+    window.location.replace("https://push-owner-patient.onrender.com/");
   }
 }
