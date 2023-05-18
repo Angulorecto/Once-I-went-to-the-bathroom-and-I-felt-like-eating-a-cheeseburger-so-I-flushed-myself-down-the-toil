8e96f903292950787fbd5809cda03310385d81bd
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-05-18 07:46:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,122 +1,44 @@
-function redirect(linkNumber) {
-   if (linkNumber == 0.1) {
-      window.location.href = "https://angulorecto.github.io/int-link/publicLinks.html";
-   }
-   if (linkNumber == 0.2) {
-      window.location.href = "https://angulorecto.github.io/int-link/premiumLinks.html";
-   }
-   if (linkNumber == 0.3) {
-      window.location.href = "https://angulorecto.github.io/int-link/memberLinks.html";
-   }
-   if (linkNumber == 1) {
-      let key = prompt("Please insert your id key.");
-      if (key == "") {
-         window.location.href = "https://angulorecto.github.io/int-link/index.html";
-      } else if (key != "Icy8X8ZSM4") {
-         window.location.href = "https://angulorecto.github.io/int-link/index.html";
-      } else {
-         window.location.href = "https://angulorecto.github.io/int-link/redirects/redirect1.html";
-      }
-   }
-   if (linkNumber == 2) {
-      let key = prompt("Please insert your id key.");
-      if (key == "") {
-         window.location.href = "https://angulorecto.github.io/int-link/index.html";
-      } else if (key != "Icy8X8ZSM4") {
-         window.location.href = "https://angulorecto.github.io/int-link/index.html";
-      } else {
-         window.location.href = "https://angulorecto.github.io/int-link/redirects/redirect2.html";
-      }
-   }
-   if (linkNumber == 3) {
-      let key = prompt("Please insert your id key.");
-      if (key == "") {
-         window.location.href = "https://angulorecto.github.io/int-link/index.html";
-      } else if (key != "Icy8X8ZSM4") {
-         window.location.href = "https://angulorecto.github.io/int-link/index.html";
-      } else {
-         window.location.href = "https://angulorecto.github.io/int-link/redirects/redirect3.html";
-      }
-   }
-   if (linkNumber == 4) {
-     window.location.href = "https://budget-consensus-unaware.onrender.com/";
-   }
-  if (linkNumber == 5) {
-    window.location.href = "https://campaign-order-just.onrender.com/";
-  }
-  if (linkNumber == 6) {
-    window.location.href = "https://head-egfc.onrender.com/";
-  }
-  if (linkNumber == 7) {
-    window.location.href = "https://overcharge.onrender.com/";
-  }
-  if (linkNumber == 8) {
-    window.location.href = "https://project-wte8.onrender.com/";
-  }
-  if (linkNumber == 9) {
-    window.location.href = "https://ignite-q595.onrender.com/";
-  }
-  if (linkNumber == 10) {
-    window.location.href = "https://certain-snap-venture-research.onrender.com/";
-  }
-  if (linkNumber == 11) {
-    window.location.replace("https://refer-perforate-extreme-loop.onrender.com/");
-  }
-  if (linkNumber == 12) {
-    window.location.replace("https://contradiction-movement-state-ghostwriter.onrender.com/");
-  }
-  if (linkNumber == 13) {
-    window.location.replace("https://assault-oppose-virtue-tree.onrender.com/");
-  }
-  if (linkNumber == 14) {
-    window.location.replace("https://accent-threshold-farewell.onrender.com/");
-  }
-  if (linkNumber == 15) {
-    window.location.replace("https://committee-myth-repeat.onrender.com/");
-  }
-  if (linkNumber == 16) {
-    window.location.replace("https://contrast-adventure-pause.onrender.com/");
-  }
-  if (linkNumber == 17) {
-    window.location.replace("https://ethan-is-cool.onrender.com/");
-  }
-  if (linkNumber == 18) {
-    window.location.replace("https://floor-confuse-sample.onrender.com/");
-  }
-  if (linkNumber == 19) {
-    window.location.replace("https://promotion-suppress-mine.onrender.com/");
-  }
-  if (linkNumber == 20) {
-    window.location.replace("https://constitution-commemorate-navy.onrender.com/");
-  }
-  if (linkNumber == 21) {
-    window.location.replace("https://exploration-federation-unfortunate.onrender.com/");
-  }
-  if (linkNumber == 22) {
-    window.location.replace("https://engineer-aunt-technology.onrender.com/");
-  }
-  if (linkNumber == 23) {
-    window.location.replace("https://undertake-recruit-instrument.onrender.com/");
-  }
-  if (linkNumber == 24) {
-    window.location.replace("https://rare-penetrate-authorise.onrender.com/");
-  }
-  if (linkNumber == 25) {
-    window.location.replace("https://promise-rugby-grudge.onrender.com/");
-  }
-  if (linkNumber == 26) {
-    window.location.replace("https://dough-lemon-despair.onrender.com/");
-  }
-  if (linkNumber == 27) {
-    window.location.replace("https://ancestor-vein-proposal.onrender.com/");
-  }
-  if (linkNumber == 28) {
-    window.location.replace("https://drift-widen-courtesy.onrender.com/");
-  }
-  if (linkNumber == 29) {
-    window.location.replace("https://side-finished-tumble.onrender.com/");
-  }
-  if (linkNumber == 30) {
-    window.location.replace("https://push-owner-patient.onrender.com/");
-  }
-}
+import express from "express";
+import http from "node:http";
+import createBareServer from "@tomphttp/bare-server-node";
+import path from "node:path";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer("/bare/");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "index.html"));
+});
+
+app.get("/public", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "publicLinks.html"));
+});
+
+app.get("/premium", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "premiumLinks.html"));
+});
+
+app.get("/member", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "memberLinks.html"));
+});
+
+app.get("/404", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "404.html"));
+});
+
+app.get("/*", (req, res) => {
+  res.redirect("/404");
+});
