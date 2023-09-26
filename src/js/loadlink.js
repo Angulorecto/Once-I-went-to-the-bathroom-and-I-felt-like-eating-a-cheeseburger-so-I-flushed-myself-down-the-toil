14af53ceb6c39d6788644b5b64d9ec1c1578c74a
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-09-26 08:20:40 -0400

    Update loadlink.js

--- src/js/loadlink.js
+++ src/js/loadlink.js
@@ -1,5 +1,7 @@
 const CryptoJS = require("crypto-js");
-var link = localStorage.getItem("hash");
-var key = "";
-const step1 = CryptoJS.AES.decrypt(link, key);
-var step2 = atob();
+function decode() {
+  var link = localStorage.getItem("hash");
+  var key = "";
+  const step1 = CryptoJS.AES.decrypt(link, key);
+  var step2 = atob();
+}
