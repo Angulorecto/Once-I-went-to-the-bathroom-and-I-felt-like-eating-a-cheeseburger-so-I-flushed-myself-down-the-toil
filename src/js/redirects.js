6ce4535de3df03ac12142ae3f5447c40339c46a4
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-09-27 15:05:13 -0400

    Update redirects.js

--- src/js/redirects.js
+++ src/js/redirects.js
@@ -219,6 +219,23 @@ function setupInKeys() {
       });
 };
 
+function decode(link) {
+  var step1 = atob(link);
+  var step2 = atob(step1);
+  var step3 = atob(step2);
+  var step4 = atob(step3);
+  var iframe = document.getElementById("iframe");
+  iframe.src = step4;
+}
+
+function storeNum(num) {
+   services.forEach((service) => {
+      if (num == service.serviceNum) {
+         localStorage.setItem("link", service.serviceNum)
+      }
+   });
+}
+
 function redirect(num) {
     pages.forEach((page) => {
        if (num == page.hashNum) {
