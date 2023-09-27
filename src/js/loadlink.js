402bc243c154971ff33a10a4f204f93d8d9ed41b
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-09-27 15:02:27 -0400

    Delete src/js/loadlink.js

--- src/js/loadlink.js
+++
@@ -1,8 +0,0 @@
-function decode(link) {
-  var step1 = atob(link);
-  var step2 = atob(step1);
-  var step3 = atob(step2);
-  var step4 = atob(step3);
-  var iframe = document.getElementById("iframe");
-  iframe.src = step4;
-}
