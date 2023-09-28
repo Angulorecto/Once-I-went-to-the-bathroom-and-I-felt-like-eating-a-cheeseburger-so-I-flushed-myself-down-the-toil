5e45aeb89d97c386ec1ef273997f1e0206bcb245
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-09-28 15:17:04 -0400

    Update redirects.js

--- src/js/redirects.js
+++ src/js/redirects.js
@@ -57,168 +57,6 @@ const pages = [
    { hashNum: "0.4", hash: "staffLinks.html" },
 ];
 
-function setupPublic() {
-   services.forEach((service) => {
-        if (service.rank == "public") {
-          var div = document.createElement('div');
-          div.setAttribute("class", "hash");
-          var x = document.createElement('img');
-          x.setAttribute("src", "../photos/BlueInLinkBadge.png");
-          x.setAttribute("width", "100");
-          x.setAttribute("alt", "InthashBadge");
-          x.setAttribute("class", "border");
-          x.setAttribute("onmouseout", "this.style.transition='width 0.5s'; this.style.width='100px';");
-          x.setAttribute("onmouseover", "this.style.transition='width 0.5s'; this.style.width='120px';");
-          x.setAttribute("onclick", "storeNum(" + service.serviceNum + ")");
-          
-          div.appendChild(x);
-          
-          var y = document.createElement('p');
-          y.innerText = service.serviceNum;
-          y.setAttribute("class", "whiteRight");
-          y.setAttribute("align", "center");
-          
-          div.appendChild(y);
-          
-          document.getElementById("row1").appendChild(div);
-        }
-      });
-};
-
-function setupPremium() {
-   services.forEach((service) => {
-        if (service.rank == "premium") {
-          var div = document.createElement('div');
-          div.setAttribute("class", "hash");
-          var x = document.createElement('img');
-          x.setAttribute("src", "../photos/GreenWeb.png");
-          x.setAttribute("width", "100");
-          x.setAttribute("alt", "InthashBadge");
-          x.setAttribute("class", "border");
-          x.setAttribute("onmouseout", "this.style.transition='width 0.5s'; this.style.width='100px';");
-          x.setAttribute("onmouseover", "this.style.transition='width 0.5s'; this.style.width='120px';");
-          x.setAttribute("onclick", "storeNum(" + service.serviceNum + ")");
-          
-          div.appendChild(x);
-          
-          var y = document.createElement('p');
-          y.innerText = service.serviceNum;
-          y.setAttribute("class", "whiteRight");
-          y.setAttribute("align", "center");
-          
-          div.appendChild(y);
-          
-          document.getElementById("row1").appendChild(div);
-        }
-      });
-};
-
-function setupMember() {
-   services.forEach((service) => {
-        if (service.rank == "member") {
-          var div = document.createElement('div');
-          div.setAttribute("class", "hash");
-          var x = document.createElement('img');
-          x.setAttribute("src", "../photos/RedBadge.png");
-          x.setAttribute("width", "100");
-          x.setAttribute("alt", "InthashBadge");
-          x.setAttribute("class", "border");
-          x.setAttribute("onmouseout", "this.style.transition='width 0.5s'; this.style.width='100px';");
-          x.setAttribute("onmouseover", "this.style.transition='width 0.5s'; this.style.width='120px';");
-          x.setAttribute("onclick", "storeNum(" + service.serviceNum + ")");
-          
-          div.appendChild(x);
-          
-          var y = document.createElement('p');
-          y.innerText = service.serviceNum;
-          y.setAttribute("class", "whiteRight");
-          y.setAttribute("align", "center");
-          
-          div.appendChild(y);
-          
-          document.getElementById("row1").appendChild(div);
-        }
-      });
-};
-
-function setupStaff() {
-   services.forEach((service) => {
-        if (service.rank == "staff") {
-          var div = document.createElement('div');
-          div.setAttribute("class", "hash");
-          var x = document.createElement('img');
-          x.setAttribute("src", "../photos/StaffLink.png");
-          x.setAttribute("width", "100");
-          x.setAttribute("alt", "InthashBadge");
-          x.setAttribute("class", "border");
-          x.setAttribute("onmouseout", "this.style.transition='width 0.5s'; this.style.width='100px';");
-          x.setAttribute("onmouseover", "this.style.transition='width 0.5s'; this.style.width='120px';");
-          x.setAttribute("onclick", "storeNum(" + service.serviceNum + ")");
-          
-          div.appendChild(x);
-          
-          var y = document.createElement('p');
-          y.innerText = service.serviceNum;
-          y.setAttribute("class", "whiteRight");
-          y.setAttribute("align", "center");
-          
-          div.appendChild(y);
-          
-          document.getElementById("row1").appendChild(div);
-        }
-      });
-};
-
-function setupAcKeys() {
-   keys.forEach((inkey) => {
-        var k = document.createElement('div');
-        k.setAttribute("align","center");
-        k.setAttribute("style","background-color:rgb(33,38,45); border-radius:15px;");
-        k.setAttribute("class","key");
-        document.getElementById("active").appendChild(k);
-        var e = document.createElement('p');
-        e.innerText = inkey.name;
-        e.setAttribute("class","whiteRight");
-        var w = document.createElement('p');
-        w.innerText = inkey.key;
-        w.setAttribute("class","greenRight");
-        var disc = document.createElement('button');
-        disc.innerHTML = "Disable";
-        disc.setAttribute("class","blackRight");
-        disc.setAttribute("style","border-radius:15px; background-color:rgb(255,0,0)");
-        disc.setAttribute("onclick","");
-        k.appendChild(e);
-        k.appendChild(w);
-        k.appendChild(disc);
-        document.body.appendChild(k);
-      });
-};
-
-function setupInKeys() {
-   inactivekeys.forEach((inkey) => {
-        var y = document.createElement('div');
-        y.setAttribute("align","center");
-        y.setAttribute("style","background-color:rgb(33,38,45); border-radius:15px;");
-        y.setAttribute("class","key");
-        document.getElementById("inactive").appendChild(y);
-        var s = document.createElement('p');
-        s.innerText = inkey.name;
-        s.setAttribute("class","whiteRight");
-        var r = document.createElement('p');
-        r.innerText = inkey.key;
-        r.setAttribute("class","redRight");
-        var dis = document.createElement('button');
-        dis.innerHTML = "Enable";
-        dis.setAttribute("class","blackRight");
-        dis.setAttribute("style","border-radius:15px; background-color:rgb(0,255,0)");
-        dis.setAttribute("onclick","");
-        y.appendChild(s);
-        y.appendChild(r);
-        y.appendChild(dis);
-        document.body.appendChild(k);
-      });
-};
-
 function load(num) {
    services.forEach((service) => {
       if (num == service.serviceNum) {
@@ -244,8 +82,14 @@ function load(num) {
                      } else {
                         alert("The key on your account isn't the correct rank to use this link.");
                      }
+                  } else if {
+                     inactiveKeys.forEach((inkey) => {
+                        if (localStorage.getItem("key") == inkey.key) {
+                           alert("Sorry the key you have on your account is inactive.\nIf you think this is a mistake go to:\nrebrand.ly/inhashrepeal");
+                        };
+                     });
                   } else {
-                     alert("The key on your account doesn't exist.");
+                     alert("They key on your account doesn't exist");
                   }
                });
             } else {
@@ -265,7 +109,7 @@ function load(num) {
                         localStorage.setItem("hash", service.hash);
                         window.location.href = "load.html";
                      } else {
-                        alert("The key on your account isn't the correct rank to use this link.");
+                        alert("The key your trying to use isn't the correct rank to use this link.");
                      }
                   } else {
                      alert("You're using a key doesn't exist.");
@@ -288,8 +132,14 @@ function load(num) {
                      } else {
                         alert("The key on your account isn't the correct rank to use this link.");
                      }
+                  } else if {
+                     inactiveKeys.forEach((inkey) => {
+                        if (localStorage.getItem("key") == inkey.key) {
+                           alert("Sorry the key you have on your account is inactive.\nIf you think this is a mistake go to:\nrebrand.ly/inhashrepeal");
+                        };
+                     });
                   } else {
-                     alert("The key on your account doesn't exist.");
+                     alert("They key on your account doesn't exist");
                   }
                });
             } else {
@@ -306,7 +156,7 @@ function load(num) {
                         localStorage.setItem("hash", service.hash);
                         window.location.href = "load.html";
                      } else {
-                        alert("The key on your account isn't the correct rank to use this link.");
+                        alert("The key you're trying to use isn't the correct rank to use this link.");
                      }
                   } else {
                      alert("You're using a key doesn't exist.");
@@ -326,8 +176,14 @@ function load(num) {
                      } else {
                         alert("The key on your account isn't the correct rank to use this link.");
                      }
+                  } else if {
+                     inactiveKeys.forEach((inkey) => {
+                        if (localStorage.getItem("key") == inkey.key) {
+                           alert("Sorry the key you have on your account is inactive.\nIf you think this is a mistake go to:\nrebrand.ly/inhashrepeal");
+                        };
+                     });
                   } else {
-                     alert("The key on your account doesn't exist.");
+                     alert("They key on your account doesn't exist");
                   }
                });
             } else {
@@ -341,7 +197,7 @@ function load(num) {
                         localStorage.setItem("hash", service.hash);
                         window.location.href = "load.html";
                      } else {
-                        alert("The key on your account isn't the correct rank to use this link.");
+                        alert("The key your're trying to use isn't the correct rank to use this link.");
                      }
                   } else {
                      alert("You're using a key doesn't exist.");
@@ -352,147 +208,3 @@ function load(num) {
       }
    });
 }
-
-function redirect(num) {
-    pages.forEach((page) => {
-       if (num == page.hashNum) {
-            window.location.href = page.hash;
-        };
-    });
-    services.forEach((service) => {
-        if (num == service.serviceNum) {
-            if (service.rank == "public") {
-                decode(service.hash);
-            };
-            if (service.rank == "premium") {
-                if (localStorage.getItem("key") != " ") {
-                    keys.forEach((key) => {
-                        if (localStorage.getItem("key") == key.key) {
-                            if (key.rank == "Premium") {
-                                window.location.href = service.hash;
-                            } else if (key.rank == "Member") {
-                                window.location.href = service.hash;
-                            } else if (key.rank == "Staff") {
-                                window.location.href = service.hash;
-                            } else if (key.rank == "Owner") {
-                                window.location.href = service.hash;
-                            } else {
-                               alert("You're trying to use a key that is at the wrong rank.\nPlease upgrade to get this rank.");
-                            };
-                        } else {
-                            inactiveKeys.forEach((inkey) => {
-                                if (localStorage.getItem("key") == inkey.key) {
-                                    alert("Sorry the key you have on your account is inactive.\nIf you think this is a mistake go to:\nrebrand.ly/inhashrepeal");
-                                };
-                            });
-                        };
-                    });
-                } else {
-                    let input = prompt("Please type in your id key.");
-                    keys.forEach((key) => {
-                        if (key.key == input) {
-                           if (key.rank == "Premium") {
-                              window.location.href = service.hash;
-                           } else if (key.rank == "Member") {
-                              window.location.href = service.hash;
-                           } else if (key.rank == "Staff") {
-                              window.location.href = service.hash;
-                           } else if (key.rank == "Owner") {
-                              window.location.href = service.hash;
-                           } else {
-                               alert("You're trying to use a key that is at the wrong rank.\nPlease upgrade to get this rank.");
-                            };
-                        };
-                    });
-                    inactiveKeys.forEach((inkey) => {
-                        if (input == inkey.key) {
-                            alert("This key is inactive.\nIf you think this is a mistake, please go to:\nrebrand.ly/inhashrepeal");
-                        };
-                    });
-                };
-            };
-            if (service.imgSrc == "pictures/RedBadge.png") {
-                if (localStorage.getItem("key") != " ") {
-                    keys.forEach((key) => {
-                        if (localStorage.getItem("key") == key.key) {
-                           if (key.rank == "Member") {
-                              window.location.href = service.hash;
-                           } else if (key.rank == "Staff") {
-                              window.location.href = service.hash;
-                           } else if (key.rank == "Owner") {
-                              window.location.href = service.hash;
-                           } else {
-                               alert("You're trying to use a key that is at the wrong rank.\nPlease upgrade to get this rank.");
-                            };
-                        } else {
-                            inactiveKeys.forEach((inkey) => {
-                                if (localStorage.getItem("key") == inkey.key) {
-                                    alert("Sorry the key you have on your account is inactive.\nIf you think this is a mistake go to:\nrebrand.ly/inhashrepeal");
-                                };
-                            });
-                        };
-                    });
-                } else {
-                    let input = prompt("Please type in your id key.");
-                    keys.forEach((key) => {
-                        if (key.key == input) {
-                            if (key.rank == "Member") {
-                                window.location.href = service.hash;
-                            } else if (key.rank == "Staff") {
-                                window.location.href = service.hash;
-                            } else if (key.rank == "Owner") {
-                                window.location.href = service.hash;
-                            } else {
-                               alert("You're trying to use a key that is at the wrong rank.\nPlease upgrade to get this rank.");
-                            };
-                        };
-                    });
-                    inactiveKeys.forEach((inkey) => {
-                        if (input == inkey.key) {
-                            alert("This key is inactive.\nIf you think this is a mistake, please go to:\nrebrand.ly/inhashrepeal");
-                        };
-                    });
-                };
-            };
-            if (service.imgSrc == "pictures/Staffhash.png") {
-                if (localStorage.getItem("key") != " ") {
-                    keys.forEach((key) => {
-                        if (localStorage.getItem("key") == key.key) {
-                           if (key.rank == "Staff") {
-                              window.location.href = service.hash;
-                           } else if (key.rank == "Owner") {
-                              window.location.href = service.hash;
-                           } else {
-                               alert("You're trying to use a key that is at the wrong rank.\nPlease upgrade to get this rank.");
-                            };
-                        } else {
-                            inactiveKeys.forEach((inkey) => {
-                                if (localStorage.getItem("key") == inkey.key) {
-                                    alert("Sorry the key you have on your account is inactive.\nIf you think this is a mistake go to:\nrebrand.ly/inhashrepeal");
-                                };
-                            });
-                        };
-                    });
-                } else {
-                    let input = prompt("Please type in your id key.");
-                    keys.forEach((key) => {
-                        if (key.key == input) {
-                            if (key.rank == "Staff") {
-                                window.location.href = service.hash;
-                            } else if (key.rank == "Owner") {
-                                window.location.href = service.hash;
-                            } else {
-                               alert("You're trying to use a key that is at the wrong rank.\nPlease upgrade to get this rank.");
-                            };
-                        };
-                    });
-                    inactiveKeys.forEach((inkey) => {
-                        if (input == inkey.key) {
-                            alert("This key is inactive.\nIf you think this is a mistake, please go to:\nrebrand.ly/inhashrepeal");
-                        };
-                    });
-                };
-            };
-        };
-    });
-};
