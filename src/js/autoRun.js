a0856b940670720bbe0a1871d283fe496a85608d
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-10-03 12:36:00 -0400

    Update autoRun.js

--- src/js/autoRun.js
+++ src/js/autoRun.js
@@ -1,7 +1,4 @@
 var hash = localStorage.getItem("hash");
 var iframe = document.getElementById("iframe");
-const step1 = aotb(hash);
-const step2 = aotb(step1);
-const step3 = aotb(step2);
-const step4 = aotb(step3);
-iframe.src = step4;
+const decode = aotb(hash);
+iframe.src = stepdecode;
