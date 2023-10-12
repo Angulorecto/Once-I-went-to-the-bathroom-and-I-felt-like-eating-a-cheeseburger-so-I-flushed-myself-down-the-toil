20be60ee709dfd592e2d36cbdfda829963bc2026
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-10-12 10:14:04 -0400

    Update codesandbox.js

--- src/js/codesandbox.js
+++ src/js/codesandbox.js
@@ -2,7 +2,7 @@ const sandboxes = [
   {src: "https://codesandbox.io/p/github/AnguloRecto2/interstellar/main?workspaceId=8fe142b0-d5eb-4938-b53a-1edafd427e66", name: "Alt"},
   {src: "https://codesandbox.io/p/github/Insider-Interstellar-Network/Interstellars/main?workspaceId=8fe142b0-d5eb-4938-b53a-1edafd427e66", name: "Insider0"},
   {src: "https://codesandbox.io/p/github/Insider-Interstellar-Network/Interstellar0.5/main?workspaceId=8fe142b0-d5eb-4938-b53a-1edafd427e66", name: "Insider0.5"},
-  {src: "", name: "Insider1"},
+  {src: "https://codesandbox.io/p/github/Insider-Interstellar-Network/Interstellar1/main?workspaceId=8fe142b0-d5eb-4938-b53a-1edafd427e66", name: "Insider1"},
   {src: "", name: "Insider1.5"},
   {src: "", name: "Insider2"},
   {src: "", name: "Insider3"},
