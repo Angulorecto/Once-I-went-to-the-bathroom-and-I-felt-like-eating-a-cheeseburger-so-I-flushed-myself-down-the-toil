493e2b08178b7b4fbe0fce656cf4ccb3183c2214
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-10-22 09:38:59 -0400

    Update setup.js

--- src/js/setup.js
+++ src/js/setup.js
@@ -159,3 +159,53 @@ function setupInKeys() {
         document.body.appendChild(k);
       });
 };
+
+function hiding() {
+   const localkey = localStorage.getItem("key");
+      const localuser = localStorage.getItem("username");
+      var account = document.getElementById("account");
+
+      if (localkey == "") {
+        account.innerHTML = "";
+      } else {
+        account.innerHTML = "Manage Account"
+      };
+      if (localuser == "") {
+        account.innerHTML = "";
+      } else {
+        account.innerHTML = "Manage Account"
+      };
+
+      keys.forEach((key) => {
+        account.setAttribute("data-key", btoa(localStorage.getItem("key")));
+        const decodedKey = account.getAttribute("data-key");
+        if (key.key == decodedKey) {
+          if (key.rank == "Premium") {
+            document.getElementById("link2").style.display = "inline-block";
+            document.getElementById("keyInfo").style.display = "none";
+          };
+          
+          if (key.rank == "Member") {
+            document.getElementById("link2").style.display = "inline-block";
+            document.getElementById("link3").style.display = "inline-block";
+            document.getElementById("keyInfo").style.display = "none";
+          };
+
+          if (key.rank == "Staff") {
+            document.getElementById("link2").style.display = "inline-block";
+            document.getElementById("link3").style.display = "inline-block";
+            document.getElementById("link4").style.display = "inline-block";
+            document.getElementById("staffArea").style.display = "block";
+            document.getElementById("keyInfo").style.display = "none";
+          };
+
+          if (key.rank == "Owner") {
+            document.getElementById("link2").style.display = "inline-block";
+            document.getElementById("link3").style.display = "inline-block";
+            document.getElementById("link4").style.display = "inline-block";
+            document.getElementById("staffArea").style.display = "block";
+            document.getElementById("keyInfo").style.display = "none";
+          };
+        };
+      });
+}
