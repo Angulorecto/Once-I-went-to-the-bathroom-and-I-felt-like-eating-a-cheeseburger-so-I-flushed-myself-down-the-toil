2f0608aaf99cbeeab069fbbdb901df55599e1d6b
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-10-23 19:58:53 -0400

    Create urlBanner.js

---
+++ src/js/urlBanner.js
@@ -0,0 +1,20 @@
+// Get the current URL
+const currentUrl = window.location.href;
+
+// Create a URLSearchParams object to parse the query parameters
+const searchParams = new URLSearchParams(currentUrl);
+
+// Get the values of the 'perma' and 'daysBanned' parameters
+const perma = searchParams.get("perma");
+const daysBanned = searchParams.get("till");
+
+// Check if the parameters exist and are not empty
+if (perma == "false") {
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + daysBanned); // Cookie expires in x days
+
+  document.cookie = "banned=true; expires=${expirationDate.toUTCString()}";
+
+} else {
+  localStorage.setItem("banned", "true");
+}
