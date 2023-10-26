ae6563ff0e131a481822e7a298682a84313bff87
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-10-26 10:10:57 -0400

    Update backKey.js

--- src/js/backKey.js
+++ src/js/backKey.js
@@ -2,7 +2,7 @@ document.addEventListener("keydown", function(event) {
   if (event.key === "`") {
     window.location.href = "home.html";
   }
-  if (event.key === "|") {
+  if (event.key === "-") {
     let prompt = prompt("What is the debug Admin key?");
     if (prompt == atob("SWN5MTM=")) {
       alert(localStorage.getItem("hash"));
