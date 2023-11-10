eb6e49d1694718b60c7278d6f83a240687871a02
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-10 12:37:31 -0500

    Update checkFor502.js

--- src/js/checkFor502.js
+++ src/js/checkFor502.js
@@ -1,4 +1,4 @@
 var frame = document.getElementById("iframeId");
-if (frame.contentWindow.document.getElementById("")) {
+if (frame.contentWindow.document.getElementByTagName("H1").innerHTML == "502") {
   frame.src = "502.html";
 }
