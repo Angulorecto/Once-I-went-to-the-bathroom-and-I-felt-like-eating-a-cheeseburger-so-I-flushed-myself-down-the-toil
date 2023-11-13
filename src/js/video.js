3ca6b4b48014ac5c61f17629cd5a6c881d0dbe2f
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-13 15:20:21 -0500

    Update video.js

--- src/js/video.js
+++ src/js/video.js
@@ -1,65 +1,13 @@
-const elmtsSrc = [
-  { type: "Storage", id: "homeVideoSrc" },
-  { type: "Storage", id: "404VideoSrc" },
-  { type: "Storage", id: "accountVideoSrc" },
-  { type: "Storage", id: "publicVideoSrc" },
-  { type: "Storage", id: "premiumVideoSrc" },
-  { type: "Storage", id: "memberVideoSrc" },
-  { type: "Storage", id: "staffVideoSrc" },
-  { type: "Element", id: "homeVidSrc" },
-  { type: "Element", id: "404vidsrc" },
-  { type: "Element", id: "accountVidSrc" },
-  { type: "Element", id: "publicVidSrc" },
-  { type: "Element", id: "premiumVidSrc" },
-  { type: "Element", id: "memberVidSrc" },
-  { type: "Element", id: "staffVidSrc" },
+const backgrounds = [
+  { }
 ]
-
-const videos = [
-  { name: "Ring of fire", src: "../videos/bg.mp4" },
-  { name: "Dust", src: "../videos/bg2.mp4" },
-]
-
-const keywords = [
-  { word: "404" },
-  { word: "account" },
-  { word: "public" },
-  { word: "premium" },
-  { word: "member" },
-  { word: "staff" },
-]
-
-function changeVideo() {
-  videos.forEach((video) => {
-    elmtsSrc.forEach((elmt) => {
-      if (elmt.type == "Input") {
-        if (document.getElementById(elmt.id).value == video.name) {
-          keywords.forEach((keyword) => {
-            if (elmt.id.includes(keyword.word) == true) {
-              elmtsSrc.forEach((elmt) => {
-                if (elmt.type == "Storage") {
-                  if (elmt.id.includes(keyword.word) == true) {
-                    localStorage.setItem(elmt.id, video.name);
-                  }
-                }
-              });
-            }
-          });
-        }
-      }
-    });
-  });
-}
-
-elmtsSrc.forEach((elmt) => {
-  if (elmt.type == "Storage") {
-    var id = localStorage.getItem(elmt.id);
-  } else {
-    var element = document.getElementById(elmt.id);
-  }
-  videos.forEach((video) => {
-    if (id == video.name) {
-      element.src = video.src;
-    }
-  }
+function changeBackground() {
+  var homevidval = document.getElementById("HomeVid").value;
+  var accountvidval = document.getElementById("AccountVid").value;
+  var publicvidval = document.getElementById("PublicVid").value;
+  var premiumvidval = document.getElementById("PremiumVid").value;
+  var membervidval = document.getElementById("MemberVid").value;
+  var staffvidval = document.getElementById("StaffVid").value;
+  var 404vidval = document.getElementById("404Vid").value;
+  
 }
