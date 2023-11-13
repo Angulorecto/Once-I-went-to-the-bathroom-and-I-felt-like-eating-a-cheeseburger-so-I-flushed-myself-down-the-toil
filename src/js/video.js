700c4ae8103a0f69a695adeab945bd65bcfd093b
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-13 08:15:25 -0500

    Create video.js

---
+++ src/js/video.js
@@ -0,0 +1,38 @@
+const elmtsSrc = [
+  { type: "Storage", id: "homeVideoSrc" },
+  { type: "Storage", id: "404VideoSrc" },
+  { type: "Storage", id: "accountVideoSrc" },
+  { type: "Storage", id: "publicVideoSrc" },
+  { type: "Storage", id: "premiumVideoSrc" },
+  { type: "Storage", id: "memberVideoSrc" },
+  { type: "Storage", id: "staffVideoSrc" },
+  { type: "Element", id: "homeVidSrc" },
+  { type: "Element", id: "404vidsrc" },
+  { type: "Element", id: "accountVidSrc" },
+  { type: "Element", id: "publicVidSrc" },
+  { type: "Element", id: "premiumVidSrc" },
+  { type: "Element", id: "memberVidSrc" },
+  { type: "Element", id: "staffVidSrc" },
+]
+
+const videos = [
+  { name: "Ring of fire", src: "../videos/bg.mp4" },
+  { name: "Dust", src: "../videos/bg2.mp4" },
+]
+
+function changeVideo() {
+  
+}
+
+elmtsSrc.forEach((elmt) => {
+  if (elmt.type == "Storage") {
+    var id = localStorage.getItem(elmt.id);
+  } else {
+    var element = document.getElementById(elmt.id);
+  }
+  videos.forEach((video) => {
+    if (id == video.name) {
+      element.src = video.src;
+    }
+  }
+}
