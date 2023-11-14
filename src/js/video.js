4363795405d00e740f2f2dced9991c4993f2ec6c
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-14 08:27:51 -0500

    Update video.js

--- src/js/video.js
+++ src/js/video.js
@@ -1,13 +1,1 @@
-const backgrounds = [
-  { }
-]
-function changeBackground() {
-  var homevidval = document.getElementById("HomeVid").value;
-  var accountvidval = document.getElementById("AccountVid").value;
-  var publicvidval = document.getElementById("PublicVid").value;
-  var premiumvidval = document.getElementById("PremiumVid").value;
-  var membervidval = document.getElementById("MemberVid").value;
-  var staffvidval = document.getElementById("StaffVid").value;
-  var 404vidval = document.getElementById("404Vid").value;
-  
-}
+
