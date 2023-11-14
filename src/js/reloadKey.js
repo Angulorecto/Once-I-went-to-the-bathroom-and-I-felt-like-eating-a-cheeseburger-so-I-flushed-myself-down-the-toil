be852b91b5cf8b52807730a7b1a2325c50c634d6
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-14 09:04:34 -0500

    Update reloadKey.js

--- src/js/reloadKey.js
+++ src/js/reloadKey.js
@@ -3,3 +3,8 @@ document.addEventListener('keydown', function(event) {
     location.reload();
   }
 });
+document.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.altKey && event.key === '`') {
+    alert(localStorage.getItem("HomeVid"));
+  }
+});
