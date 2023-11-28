9152ad4105a973ee3e841a0923e6ffd8594858fc
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-28 14:58:56 -0500

    Update keyed.js

--- src/js/keyed.js
+++ src/js/keyed.js
@@ -6,14 +6,20 @@
         account.setAttribute("data-key", btoa(localStorage.getItem("key")));
         const decodedKey = account.getAttribute("data-key");
         if (key.key == decodedKey) {
+          if (key.rank == "Roblox") {
+            document.getElementById("link5").style.display = "inline-block";
+            document.getElementById("keyInfo").style.display = "none";
+          }
           if (key.rank == "Premium") {
             document.getElementById("link2").style.display = "inline-block";
+            document.getElementById("link5").style.display = "inline-block";
             document.getElementById("keyInfo").style.display = "none";
           };
           
           if (key.rank == "Member") {
             document.getElementById("link2").style.display = "inline-block";
             document.getElementById("link3").style.display = "inline-block";
+            document.getElementById("link5").style.display = "inline-block";
             document.getElementById("keyInfo").style.display = "none";
           };
 
@@ -21,6 +27,7 @@
             document.getElementById("link2").style.display = "inline-block";
             document.getElementById("link3").style.display = "inline-block";
             document.getElementById("link4").style.display = "inline-block";
+            document.getElementById("link5").style.display = "inline-block";
             document.getElementById("staffArea").style.display = "block";
             document.getElementById("keyInfo").style.display = "none";
           };
@@ -29,6 +36,7 @@
             document.getElementById("link2").style.display = "inline-block";
             document.getElementById("link3").style.display = "inline-block";
             document.getElementById("link4").style.display = "inline-block";
+            document.getElementById("link5").style.display = "inline-block";
             document.getElementById("staffArea").style.display = "block";
             document.getElementById("keyInfo").style.display = "none";
             document.getElementById("keyMgr").style.display = "block";
