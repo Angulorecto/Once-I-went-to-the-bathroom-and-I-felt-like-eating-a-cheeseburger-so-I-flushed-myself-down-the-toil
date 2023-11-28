35b4c41d7d0c797eabed9881dd3c8ede877b4aab
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-28 15:29:18 -0500

    Update autoRun.js

--- src/js/autoRun.js
+++ src/js/autoRun.js
@@ -2,7 +2,7 @@ var hash = localStorage.getItem("hash");
 var iframe = document.getElementById("iframeId");
 const decode = atob(hash);
 if (localStorage.getItem("Roblox") == "true") {
-   iframe.src = decode + "/reviews/hvtrs8%2F-nmw%2Cge%2Frlcy-rmbnoz-aoppmrctkol%2F7369-rmbnoz";
+   iframe.src = decode + "reviews/hvtrs8%2F-nmw%2Cge%2Frlcy-rmbnoz-aoppmrctkol%2F7369-rmbnoz";
    localStorage.setItem("Roblox", "false");
 } else {
    iframe.src = decode;
