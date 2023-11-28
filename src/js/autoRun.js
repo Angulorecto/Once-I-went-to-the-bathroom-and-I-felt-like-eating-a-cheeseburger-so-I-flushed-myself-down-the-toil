9ca6de57ef55e5b7af3b9c3885d2da8a541ea5d8
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-28 15:19:59 -0500

    Update autoRun.js

--- src/js/autoRun.js
+++ src/js/autoRun.js
@@ -1,7 +1,12 @@
 var hash = localStorage.getItem("hash");
 var iframe = document.getElementById("iframeId");
 const decode = atob(hash);
-iframe.src = decode;
+if (localStorage.getItem("Roblox") == "true") {
+   iframe.src = decode + "";
+   localStorage.setItem("Roblox", "false");
+} else {
+   iframe.src = decode;
+}
 
 function back() {
    window.location.href = "home.html";
