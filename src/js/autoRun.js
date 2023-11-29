30000e909998dacc93ba10ca21cf9fc5dd915b4b
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-11-29 09:14:02 -0500

    Update autoRun.js

--- src/js/autoRun.js
+++ src/js/autoRun.js
@@ -3,7 +3,6 @@ var iframe = document.getElementById("iframeId");
 const decode = atob(hash);
 iframe.src = decode;
 if (localStorage.getItem("roblox") == "true") {
-   iframe.contentWindow.images("https://now.gg/play/roblox-corporation/5349/roblox");
    localStorage.setItem("roblox", "false");
 }
 
