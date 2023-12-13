23107732608427bf9b26af612a65a6bb9103c3a4
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-12-13 10:50:42 -0500

    Update iconsHover.js

--- src/js/iconsHover.js
+++ src/js/iconsHover.js
@@ -1,4 +1,17 @@
-function startAnimation(elementId) {
+function removeText(text, animationTag) {
+    // Input string
+    let originalText = text;
+    // Replace method to remove given text
+    let newText = originalText.replace(animationTag, '');
+     
+    // Display output
+    console.log(newText);
+}
+function startAnimation(elementId, aniTag) {
   var currentClass = document.getElementById(elementId).class;
-  document.getElementById(elementId).class = "";
+  currentClass + aniTag;
+}
+function stopAnimation(elementId, aniTag) {
+  var currentClass = document.getElementById(elementId).class;
+  currentClass + aniTag;
 }
