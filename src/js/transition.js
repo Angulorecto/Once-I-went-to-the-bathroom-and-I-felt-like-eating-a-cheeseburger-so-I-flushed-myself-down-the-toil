9987a80cc146c1600898adaeb5fff23d70ad2e3d
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-12-18 14:57:34 -0500

    Update transition.js

--- src/js/transition.js
+++ src/js/transition.js
@@ -1,5 +1,11 @@
+function blueDone() {
+  document.getElementById("box1").style.bottom = "50%";
+  document.getElementById("box2").style.top = "50%";
+  document.getElementById("blueBadge").style.opacity = "100%";
+}
 function blue() {
   document.getElementById("box1").style.animation = "down 4s";
   document.getElementById("box2").style.animation = "up 4s";
   document.getElementById("blueBadge").style.animation = "fade 4s";
+  
 }
