3bb70130eb9b77477ffbb2bde96d53dce67f8589
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-12-18 15:13:01 -0500

    Update transition.js

--- src/js/transition.js
+++ src/js/transition.js
@@ -4,8 +4,9 @@ function blueDone() {
   document.getElementById("blueBadge").style.opacity = "100%";
 }
 function blue() {
-  document.getElementById("box1").style.animation = "down 3.9s";
-  document.getElementById("box2").style.animation = "up 3.9s";
-  document.getElementById("blueBadge").style.animation = "fade 3.9s";
-  setTimeout(blueDone, 3750)
+  document.getElementById("box1").style.animation = "down 3s";
+  document.getElementById("box2").style.animation = "up 3s";
+  document.getElementById("blueBadge").style.animation = "fade 3s";
+  // Note: this line's timing has to be 0.15 less then the 3 above.
+  setTimeout(blueDone, 2850)
 }
