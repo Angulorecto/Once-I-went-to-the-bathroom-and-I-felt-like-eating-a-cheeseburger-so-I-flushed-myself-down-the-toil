41fb91bc99a2b0ca2fc0eebfde54b7ea2ece2d8c
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-12-18 15:25:25 -0500

    Update redirects.js

--- src/js/redirects.js
+++ src/js/redirects.js
@@ -82,6 +82,18 @@ function checkout(rank) {
 function send() {
    window.location.href = "load.html";
 }
+function blueDone() {
+  document.getElementById("box1").style.bottom = "50%";
+  document.getElementById("box2").style.top = "50%";
+  document.getElementById("blueBadge").style.opacity = "100%";
+}
+function blue() {
+  document.getElementById("box1").style.animation = "down 3s";
+  document.getElementById("box2").style.animation = "up 3s";
+  document.getElementById("blueBadge").style.animation = "fade 3s";
+  // Note: this line's timing has to be 0.15 less then the 3 above.
+  setTimeout(blueDone, 2850)
+}
 function redirect(num) {
     pages.forEach((page) => {
        if (num == page.linkNum) {
