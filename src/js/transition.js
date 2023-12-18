d274634483457a3aa42034997e5f8df6064cbcac
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-12-18 10:11:26 -0500

    Update transition.js

--- src/js/transition.js
+++ src/js/transition.js
@@ -1,4 +1,5 @@
 function blue() {
   document.getElementById("box1").style.animation = "down 2s";
   document.getElementById("box2").style.animation = "up 2s";
+  document.getElementById("blueBadge").style.animation = "fade 2s";
 }
