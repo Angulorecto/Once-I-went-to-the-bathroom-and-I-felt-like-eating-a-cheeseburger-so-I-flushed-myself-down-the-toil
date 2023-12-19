5802211a81ad3fc19bc168dc8f37da951468b573
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-12-19 17:44:54 -0500

    Update transition.js

--- src/js/transition.js
+++ src/js/transition.js
@@ -9,6 +9,7 @@ function blue() {
   document.getElementById("box1").style.display = "block";
   document.getElementById("box2").style.display = "block";
   document.getElementById("blueBadge").style.animation = "fade 3s";
+  document.getElementById("blueBadge").style.display = "block";
   // Note: this line's timing has to be 0.15 less then the 3 above.
   setTimeout(blueDone, 2850)
 }
