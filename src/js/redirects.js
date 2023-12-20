c5ea8430444f982336cf43f6008e66484a987dbb
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-12-20 10:22:43 -0500

    Update redirects.js

--- src/js/redirects.js
+++ src/js/redirects.js
@@ -80,12 +80,11 @@ function setupPublic() {
    services.forEach((service) => {
         if (service.rank == "public") {
           var div = document.createElement('div');
-          div.setAttribute("class", "link");
+          div.setAttribute("style", "display:inline-block;");
           var x = document.createElement('img');
           x.setAttribute("src", "../photos/BlueInLinkBadge.png");
           x.setAttribute("alt", "InthashBadge");
           x.setAttribute("class", "border");
-          x.setAttribute("style", "width:100px;");
           x.setAttribute("onclick", "redirect(" + service.serviceNum + ")");
           
           div.appendChild(x);
@@ -96,11 +95,8 @@ function setupPublic() {
           y.setAttribute("align", "center");
           
           div.appendChild(y);
-          if (service.serviceNum < 50) {
-             document.getElementById("row1").appendChild(div);
-          } else {
-             document.getElementById("row2").appendChild(div);
-          }
+          
+          document.getElementById("row1").appendChild(div);
         }
       });
 };
@@ -109,11 +105,10 @@ function setupPremium() {
    services.forEach((service) => {
         if (service.rank == "premium") {
           var div = document.createElement('div');
-          div.setAttribute("class", "link");
+          div.setAttribute("style", "display:inline-block;");
           var x = document.createElement('img');
           x.setAttribute("src", "../photos/GreenWeb.png");
           x.setAttribute("alt", "InthashBadge");
-          x.setAttribute("style", "width:100px;");
           x.setAttribute("class", "border");
           x.setAttribute("onclick", "redirect(" + service.serviceNum + ")");
           
@@ -135,11 +130,10 @@ function setupMember() {
    services.forEach((service) => {
         if (service.rank == "member") {
           var div = document.createElement('div');
-          div.setAttribute("class", "link");
+          div.setAttribute("style", "display:inline-block;");
           var x = document.createElement('img');
           x.setAttribute("src", "../photos/RedBadge.png");
           x.setAttribute("alt", "InthashBadge");
-          x.setAttribute("style", "width:100px;");
           x.setAttribute("class", "border");
           x.setAttribute("onclick", "redirect(" + service.serviceNum + ")");
           
@@ -161,18 +155,17 @@ function setupStaff() {
    services.forEach((service) => {
         if (service.rank == "staff") {
           var div = document.createElement('div');
-          div.setAttribute("class", "link");
+          div.setAttribute("style", "display:inline-block;");
           var x = document.createElement('img');
           x.setAttribute("src", "../photos/StaffLink.png");
           x.setAttribute("alt", "InthashBadge");
-          x.setAttribute("style", "width:100px;");
           x.setAttribute("class", "border");
           x.setAttribute("onclick", "redirect(" + service.serviceNum + ")");
           
           div.appendChild(x);
           
           var y = document.createElement('p');
-          y.innerText = service.serviceNum;
+          y.innerText = "Roblox";
           y.setAttribute("class", "whiteRight");
           y.setAttribute("align", "center");
           
