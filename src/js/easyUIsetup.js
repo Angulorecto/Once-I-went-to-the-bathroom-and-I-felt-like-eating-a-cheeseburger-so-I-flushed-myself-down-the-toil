56d3f9d8ce26c0a67a00dc2d3e6c57f15e2c59b1
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-12-22 08:15:44 -0500

    Create easyUIsetup.js

---
+++ src/js/easyUIsetup.js
@@ -0,0 +1,26 @@
+function colorTitle2(color1, color2) {
+  const s1 = document.createElement("span");
+  s1.style.font-family = "righty";
+  const s2 = document.createElement("span");
+}
+function colorTitle3(color1, color2, color3) {
+  
+}
+function colorTitle4(color1, color2, color3, color4) {
+  
+}
+function colorTitle5(color1, color2, color3, color4, color5) {
+  
+}
+function colorTitle6(color1, color2, color3, color4, color5, color6) {
+  
+}
+function colorTitle7(color1, color2, color3, color4, color5, color6, color7) {
+  
+}
+function colorTitle8(color1, color2, color3, color4, color5, color6, color7, color8) {
+  
+}
+function colorTitle9(color1, color2, color3, color4, color5, color6, color7, color8, color9) {
+  
+}
