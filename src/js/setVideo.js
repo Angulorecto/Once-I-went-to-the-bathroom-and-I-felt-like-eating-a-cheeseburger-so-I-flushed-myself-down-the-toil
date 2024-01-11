a7496135e3741a2adffb2100d5afa07cf0997898
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-01-11 08:53:56 -0500

    Update setVideo.js

--- src/js/setVideo.js
+++ src/js/setVideo.js
@@ -1,3 +1,1 @@
-var srcVid = "../videos/Dust Particles pack (online-video-cutter.com).mp4";
-var srcArea = document.getElementById("VidSrc");
-srcArea.src = srcVid;
+document.getElementById("VidSrc").src = "../videos/Dust Particles pack (online-video-cutter.com).mp4";
